fee6aa39fc23a72978362b94bad285ef135cebbb
Stephen Blevins <scblevins27@gmail.com>
2021-12-13 01:24:31 -0500

    added modal

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-const results = document.querySelector('.results');
+const signIn = document.querySelector('.sign-in');
+const modal = document.querySelector('.modal-container');
+const submit = document.querySelector('.submit');
+const exitButton = document.querySelector('.exit-button');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const emailReq = document.querySelector('.email-required');
+const passReq = document.querySelector('.password-required');
 
-results.innerHTML = 'hello'
+let verifyInputs = () => {
+    return true;
+}
+
+signIn.addEventListener('click', () => {
+    modal.classList.add('visible');
+
+})
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (verifyInputs()) {
+        modal.classList.remove('visible');
+    }
+})
+
+exitButton.addEventListener('click', () => {
+    modal.classList.remove('visible');
+})
+
+
+
